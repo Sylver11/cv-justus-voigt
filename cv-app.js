493b45ac2c264e5b530a59eb998773a27a4f9d05
Sylver11 <justus@cognician.com>
2020-03-19 13:22:47 +0000

    changing defautl directory path

--- cv-app.js
+++ cv-app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 // const path = require('path');
-app.use(express.static('/public'));
+app.use(express.static(__dirname));
 app.listen(2000, () => console.log('Server is ready and listens at 2000'))
